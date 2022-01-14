9f4d1c10c22ad0b137bd7c403b53732ad89fd56f
EduardoHS-ZWrld <eduardohs29@gmail.com>
2022-01-14 11:46:44 -0300

    Módulo 8 - Angular Reactive Forms (pt.2)

--- Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
+++ Front/ProEventos-App/src/app/components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit() {
+  get f(): any {
+    return this.form.controls;
   }
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.validation();
+  }
+
+  validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('novaSenha', 'novaSenhaConfirm')
+    };
+
+    this.form = this.fb.group({
+      titulo: ['', Validators.required],
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(11)]],
+      funcao: ['', Validators.required],
+      descUsuario: ['', Validators.maxLength(100)],
+      novaSenha: ['', [Validators.minLength(6), Validators.maxLength(25)]],
+      novaSenhaConfirm: ['']
+    }, formOptions);
+  }
 }
