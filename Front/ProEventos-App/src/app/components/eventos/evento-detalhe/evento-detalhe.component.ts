ee5e77e7e170cd1601990a61a1de0d42492375dd
EduardoHS-ZWrld <eduardohs29@gmail.com>
2022-01-14 11:45:34 -0300

    Módulo 8 - Angular Reactive Forms (pt.1)

--- Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventoDetalheComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validation();
+  }
+
+  public validation(): void {
+    this.form = this.fb.group({
+      tema: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(50)]],
+      local: ['', Validators.required],
+      dataEvento: ['', Validators.required],
+      qtdPessoas: ['', [Validators.required, Validators.min(1), Validators.max(120000)]],
+      telefone: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(11)]],
+      email: ['', [Validators.required, Validators.email]],
+      imagemURL: ['', Validators.required],
+    });
+  }
+
+  public resetForm(): void {
+    this.form.reset();
   }
 
 }
--- Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ Front/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from '@app/helpers/ValidatorField';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validation();
+  }
+
+  validation(): void {
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'senhaConfirm')
+    };
+
+    this.form = this.fb.group({
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      usuarioNome: ['', Validators.required],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      senhaConfirm: ['', Validators.required]
+    }, formOptions);
   }
 
 }
